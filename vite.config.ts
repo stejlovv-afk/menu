f17c4f2fc8c4c67a3538544c94748345a6523f98
stejlovv-afk <stejlovv@gmail.com>
2025-12-28 21:52:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Мы меняем '/menu/' на './', чтобы приложение работало и на GitHub, и на IP сервера
-  base: './', 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    // Это поможет избежать проблем с путями к картинкам
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-    host: true
-  }
-});
+  base: './', // ВАЖНО: именно относительный путь убирает белый экран на GitHub Pages
+})
