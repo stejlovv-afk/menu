1c9df89ecd936ef54b13e5ca791488fdd7a635e9
stejlovv-afk <stejlovv@gmail.com>
2025-12-28 17:29:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ВАЖНО: Это название вашего репозитория. Если репозиторий называется 'menu', оставьте как есть.
-  // Если по-другому, поменяйте '/menu/' на '/ваше-название/'.
   base: '/menu/', 
   build: {
     outDir: 'dist',
+    sourcemap: true
   }
 })
