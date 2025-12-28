c3b0bcee72e95b4baf8a63b2ca8a57dcc8241553
stejlovv-afk <stejlovv@gmail.com>
2025-12-28 19:31:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/menu/', 
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+  base: '/menu/', // Это самое важное исправление для GitHub Pages
 })
